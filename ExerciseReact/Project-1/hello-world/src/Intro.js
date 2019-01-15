859b0c80b4185be1a2bf72cd7d72ca893a5856f9
Rajendra Furde <rajendra.furde@weboniselab.com>
2019-01-15 11:17:11 +0530

    Imported components from other files

---
+++ ExerciseReact/Project-1/hello-world/src/Intro.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class Intro extends Component {
+  render() {
+    return (
+      <div>
+         <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+      </div>
+    )
+  }
+}
+
+
+export default Intro;
