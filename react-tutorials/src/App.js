aa5ea7aecfa395c0c97dd6a91b0c4afd8de2e04a
Rajendra Furde <webrajendra@gmail.com>
2023-01-26 16:27:49 +0530

    First commit for props demo

---
+++ react-tutorials/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Student from './components/Students';
+
+function App() {
+  return (
+    <div className="App">
+       <Student nickName="Raja" age={34} />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-tutorials/src/components/Movie.jsx
@@ -0,0 +1,7 @@
+const Movie = () => {
+    return(
+        <>
+            <p>Name - </p>
+        </>
+    )
+}
