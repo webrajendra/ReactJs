7e2c38a033ecaffecbb117e8ac4e1d2b2649fa35
Rajendra Furde <webrajendra@gmail.com>
2023-02-05 17:40:39 +0530

    Added back button using usenavigate

--- static-web-app-routing/src/components/Error.js
+++ static-web-app-routing/src/components/Error.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function Error() {
+    const navigate = useNavigate();
   return (
-    <h1>404 Page Not Found.</h1>
+    <>
+        <h1>404 Page Not Found.</h1>
+        <button onClick={() => navigate(-1)}>Go Back</button>
+    </>
   )
 }
 
